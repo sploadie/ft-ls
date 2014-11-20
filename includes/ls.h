3cca41ed683955f04df33211469ca36b5c2627b6
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2014-11-20 17:38:58 +0100

    Added permitted system libraries

--- includes/ls.h
+++ includes/ls.h
@@ -6,7 +6,43 @@
 /*   By: tgauvrit <tgauvrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/20 17:12:51 by tgauvrit          #+#    #+#             */
-/*   Updated: 2014/11/20 17:18:09 by tgauvrit         ###   ########.fr       */
+/*   Updated: 2014/11/20 17:38:04 by tgauvrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LS_H
+# define LS_H
+
+# include <unistd.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <uuid/uuid.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+/*
+** Permitted functions:
+** write
+** opendir
+** readdir
+** closedir
+** stat
+** lstat
+** getpwuid
+** getgrgid
+** listxattr
+** getxattr
+** time
+** ctime
+** readlink
+** malloc
+** free
+** perror
+** strerror
+** exit
+*/
+
+#endif
